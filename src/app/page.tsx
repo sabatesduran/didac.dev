d27071d162302dffa2bc2d41d1b73c5ca82ca2f9
Dídac Sabatés <sabatesduran@gmail.com>
2023-09-08 13:40:19 +0200

    fix home link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
     <>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between px-10 py-16 md:px-24 md:py-24">
-        <div className="relative flex flex-col before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="relative flex flex-col before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
           <T.H1 className="place-items-center">Dídac Sabatés</T.H1>
           <T.P>
             Full Stack Developer from Barcelona currently{" "}
             <span className="font-bold">looking for new opportunities</span>{" "}
             (look{" "}
-            <Link href="cv/" className="underline">
+            <Link href="/cv" className="underline">
               here
             </Link>
             , to know more about my work experience).
