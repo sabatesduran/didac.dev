344feac93e1d5b5975ab7f78b9b874099dcb6fd1
Dídac Sabatés <sabatesduran@gmail.com>
2023-09-28 09:52:05 +0200

    add redirects

--- next.config.js
+++ next.config.js
@@ -12,6 +12,40 @@ const nextConfig = {
   images: {
     domains: ["source.unsplash.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/x',
+        destination: 'https://x.com/didacsd',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/sabatesduran',
+        permanent: true,
+      },
+      {
+        source: '/gitlab',
+        destination: 'https://gitlab.com/sabatesduran',
+        permanent: true,
+      },
+      {
+        source: '/printables',
+        destination: 'https://www.printables.com/@didac',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/didacsabates/',
+        permanent: true,
+      },
+      {
+        source: '/espiagasolineras',
+        destination: 'https://espiagasolineras.com',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 const withMDX = require("@next/mdx")();
