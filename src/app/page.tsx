b1694f17b2e570f4f1c9f0441c3d59a1852d5400
Dídac Sabatés <sabatesduran@gmail.com>
2023-10-27 15:26:51 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         <div className="relative flex flex-col before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
           <T.H1 className="place-items-center">Dídac Sabatés</T.H1>
           <T.P>
-            Full Stack Developer from Barcelona currently{" "}
-            <span className="font-bold">looking for new opportunities</span>{" "}
+            Full Stack Engineer from Barcelona currently working at{" "}
+            <span className="font-bold">Abacum</span>{" "}
             (look{" "}
             <Link href="/cv" className="underline">
               here
