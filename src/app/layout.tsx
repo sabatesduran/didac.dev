e03c5f2799d889a4f498a381c1ea03e9e516886e
Dídac Sabatés <sabatesduran@gmail.com>
2023-11-20 21:33:55 +0100

    tinybird

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       {!isDev() && (
-        <Script
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "2148d9da1cbb473c8f74ac25c731d499"}'
-        />
+        <>
+          <Script
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "2148d9da1cbb473c8f74ac25c731d499"}'
+          />
+          <Script
+            src="https://unpkg.com/@tinybirdco/flock.js"
+            data-host="https://api.tinybird.co"
+            data-token="p.eyJ1IjogIjg1ZDUxNWYzLWY4YmMtNDliNi1iZWU3LTFkOWM2NjZkYzlhMiIsICJpZCI6ICI3MTg5NzM2Ni0xMzY4LTQwNTYtYWQ5Yi1jMmRkZTdmYjRlYmQiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.z-eljb7LypZBNcaodzsZb_8mJDtYIblV3rDZswwEbzE"
+          />
+        </>
       )}
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
